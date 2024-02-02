cb9856c0871d8027490e9ec2aa4cf5c983b343dc
Rob <rob.marjot@gmail.com>
2024-02-02 12:42:19 +0100

    ARC MODAPE run helper script (bash): use quoted path in volume binding

--- chain/utils/arc_modape_run.sh
+++ chain/utils/arc_modape_run.sh
@@ -2,5 +2,5 @@
 #
 # Runs the provided script inside the container. Usage: ./arc_modape_run.sh policy_dataset_test.sh
 #
-docker run -v $PWD:/arc_modape_chain -v /var/storage:/var/storage \
+docker run -v "$(pwd):/arc_modape_chain" -v "$(pwd)/storage:/var/storage" \
   --rm -it arc-modape bash -c '/arc_modape_chain/$0 |& tee -a /var/storage/modape_$(date -d "today" +"%Y%m%d%H%M").log' $1
