f7afa02f999b6ade6d8f39bd9d174eab71c27171
interob <rob.marjot@gmail.com>
2024-10-30 17:35:44 +0000

    WIP: in-container reproduction

---
+++ development/reproduce/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Use this little script to setup an output directory for running the reproduction script
+# within your development container:
+cd /var/storage/ \
+ && mkdir arc_modape_ndvi \
+ && cd arc_modape_ndvi
+
+ln -s /var/modape/modape/scripts/modis_download.py .
+ln -s /var/modape/modape/scripts/modis_collect.py .
+ln -s /var/modape/modape/scripts/modis_smooth.py .
+ln -s /var/modape/modape/scripts/modis_window.py .
